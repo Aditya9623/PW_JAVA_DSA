76ecd9c03544b946497d93738de7ca7a4b4970ae
Aditya Vishwakarma <155947838+Aditya9623@users.noreply.github.com>
2024-10-31 16:09:52 +0530

    Add files via upload

---
+++ LinkedList1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class LinkedList1
+{
+	public static void main(String[] args) {
+		ArrayList l=new ArrayList();
+		l.add(10);
+		l.add("as");
+		System.out.print(l);
+	}
+}
